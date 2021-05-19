14e148aa0bbfdfae036be55e15790ef2ade77e53
Jindřich Máca <tunik93@gmail.com>
2021-05-19 23:09:14 +0200

    💄 Implement simple header with Material-UI

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,19 @@
 import { FunctionComponent } from 'preact'
+import { Box, Typography } from '@material-ui/core'
 
 import { Logo } from '../Logo'
 
 export const App: FunctionComponent = () => (
-	<>
+	<Box
+		component="header"
+		my="2em"
+		display="flex"
+		flexDirection="column"
+		alignItems="center"
+	>
 		<Logo />
-		<h1>Walk The Dog!</h1>
-	</>
+		<Typography variant="h1" align="center">
+			Walk The Dog!
+		</Typography>
+	</Box>
 )

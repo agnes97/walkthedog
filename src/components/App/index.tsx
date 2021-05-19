761c1ea89c8982fda6c7aa4a0aa41eb4eee77156
Jindřich Máca <tunik93@gmail.com>
2021-05-19 20:46:42 +0200

    🗑 Clear project template code

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -5,16 +5,6 @@ import { Logo } from '../Logo'
 export const App: FunctionComponent = () => (
 	<>
 		<Logo />
-		<p>Hello Vite + Preact!</p>
-		<p>
-			<a
-				className="link"
-				href="https://preactjs.com/"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Learn Preact
-			</a>
-		</p>
+		<h1>Walk The Dog!</h1>
 	</>
 )
